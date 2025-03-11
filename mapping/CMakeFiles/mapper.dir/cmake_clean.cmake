17ca8e2f454522800da5cbe8d178872d9db770cd
James-reynaldo3012 <jamesreynaldo527@gmail.com>
2025-03-11 15:08:43 +0100

    Add files via upload

---
+++ mapping/CMakeFiles/mapper.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "/media/sf_robotics_c/workspace_assignment4/workspace_assignment4/devel/lib/mapping/mapper"
+  "/media/sf_robotics_c/workspace_assignment4/workspace_assignment4/devel/lib/mapping/mapper.pdb"
+  "CMakeFiles/mapper.dir/src/MappingNode.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/mapper.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
