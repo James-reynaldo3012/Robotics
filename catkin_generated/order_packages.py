b00ce3092f8ca5ff6a3d171f0d19c2bd44a7ffaa
James-reynaldo3012 <jamesreynaldo527@gmail.com>
2025-03-11 15:01:57 +0100

    Add files via upload

---
+++ catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/media/sf_robotics_c/workspace_assignment4/workspace_assignment4/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/media/sf_robotics_c/workspace_assignment4/workspace_assignment4/devel;/opt/ros/noetic'.split(';') if '/media/sf_robotics_c/workspace_assignment4/workspace_assignment4/devel;/opt/ros/noetic' != '' else []
---
+++ catkin_generated/setup_cached.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export PWD='/media/sf_robotics_c/workspace_assignment4/workspace_assignment4/build'
